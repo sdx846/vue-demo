34f6b8fda44287774d20acc977199c406c68117a
sdx846 <sdx_liuhui@163.com>
2020-07-28 10:22:54 +0800

    sourceMap

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ module.exports = {
 
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
+    devtool: '#source-map',//'#source-map',"none"
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
