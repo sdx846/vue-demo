8ae6c0c9a110d4597b38831c0abfd9f125ad5c22
sdx846 <sdx_liuhui@163.com>
2021-12-15 16:19:09 +0800

    自定义select

--- src/common/cus/CSelectJs.vue
+++ src/common/cus/CSelectJs.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="c-select">
-    <div
-      :class="{'select-text':true,'border-active':showSizeOpts}"
-      @click.stop="onShowSize($event)"
-    >
-      <span>{{ pageSize }}条/页</span>
-      <span :class="showSizeOpts?'top-arrow size-arrow':'bottom-arrow size-arrow'">&lt;</span>
+  <div class="c-select" :style="'height:'+height+'px;'">
+    <div :class="{'select-text':true,'border-active':showOpts}" @click.stop="onShowOpts($event)">
+      <span>{{ selectLabel }}</span>
+      <span :class="showOpts?'top-arrow size-arrow':'bottom-arrow size-arrow'">&lt;</span>
     </div>
-    <div
-      class="select-opts"
-      :style="'top:'+top+'px;max-height:'+height*4+'px;'"
-      v-if="showSizeOpts"
-    >
+    <div class="select-opts" :style="'top:'+top+'px;max-height:'+height*4+'px;'" v-if="showOpts">
       <div
-        :class="{'select-opt':true,'activeSize':item === pageSize}"
+        :class="{'select-opt':true,'activeSize':item.value === selectVal}"
         :style="'height:'+height+'px;'"
-        v-for="item in pageSizeOpts"
-        :key="item"
-        @click="onSelectSize(item)"
-      >{{ item }}条/页</div>
+        v-for="(item,index) in opts"
+        :key="index"
+        @click="onSelectOpt(item.value)"
+      >{{ item.label }}</div>
     </div>
   </div>
 </template>
@@ -26,26 +19,84 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      showOpts: false,
+      selectLabel: ''
+    };
   },
-  components: {},
-  created() {},
-  mounted() {},
-  methods: {}
+  props: {
+    opts: {
+      type: Array,
+      default: []
+    },
+    selectVal: {
+      type: String,
+      default: ''
+    },
+    height: {
+      type: Number,
+      default: 30
+    }
+  },
+  // computed: {
+  //    selectLabel() {
+  //     return this.opts.filter(x=>x.value ===this.selectVal)[0].label
+  //   }
+  // },
+  watch: {
+    // selectVal(newVal,oldVal){
+    //   this. selectLabel = this.opts.filter(x=>x.value === newVal)[0].label
+
+    // }
+    selectVal: {
+      handler: function(val) {
+        this.selectLabel = this.opts.filter(x => x.value === val)[0].label;
+      },
+      deep: true,
+      immediate: true
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.onHideOpts);
+    window.addEventListener('click', this.onHideOpts);
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.onHideOpts);
+    window.removeEventListener('click', this.onHideOpts);
+  },
+  methods: {
+    onHideOpts() {
+      this.showOpts = false;
+    },
+    onShowOpts(e) {
+      const len = this.opts.length > 5 ? 5 : this.opts.length;
+      const h = this.height * len;
+      this.top = window.innerHeight - e.y > h ? this.height : -h;
+      this.showOpts = !this.showOpts;
+    },
+    onSelectOpt(opt) {
+      this.$emit('change', opt);
+    }
+  }
 };
 </script>
 
 <style scoped lang="scss">
+@import '../../styles/common.scss';
+$borderColor: #ddd;
+$activeColor: #cac7c7;
+$hoverBgColor: #e6f7ff;
+$disabledColor: #d6d3d3;
 .c-select {
   position: relative;
   margin: 0 8px;
-  width: 90px;
-  height: 100%;
+  width: 120px;
 
   .select-text {
     height: 100%;
     border: 1px solid $borderColor;
     padding: 0 8px;
+    border-radius: 4px;
     @extend %flex-x-b;
     .size-arrow {
       color: $borderColor;
--- src/components/systemManage/form/cusSelect.vue
+++ src/components/systemManage/form/cusSelect.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="">
-  
+  <div class="select-page">
+    <c-select style="width:160px;height:32px;" :opts="opts" :selectVal="selectVal" @change="changeOk"></c-select>
   </div>
 </template>
 
@@ -9,20 +9,29 @@
 export default {
   data() {
     return {
+      opts: [
+        { label: '苹果1', value: 'apple1' },
+        { label: '苹果2', value: 'apple2' },
+        { label: '苹果3', value: 'apple3' },
+        { label: '苹果4', value: 'apple4' },
+        { label: '苹果5', value: 'apple5' },
+        { label: '苹果6', value: 'apple6' }
+      ],
+      selectVal: 'apple3'
     };
   },
-  components: {
-  },
-  created() {
-  },
-  mounted() {
-
-  },
   methods: {
+    changeOk(val) {
+      this.selectVal = val;
+    }
   }
 };
 </script>
 
 <style scoped>
-
+.select-page{
+    padding:20px;
+    color:#333;
+    font-size:14px;
+}
 </style>
